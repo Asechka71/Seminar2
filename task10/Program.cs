335aa18dce69f52a6e2566beaa877a2d890ca520
Anastasiia <kariakina.nastia@example.com>
2022-08-11 15:23:16 +0300

    add Task 10

---
+++ task10/Program.cs
@@ -0,0 +1,10 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа. */
+
+Console.WriteLine("Введите трехзначное число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int number1 = number / 10;
+int SecondDigital = number1 % 10;
+
+Console.WriteLine($"Вторая цифра числа: {SecondDigital}");
